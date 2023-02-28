beda6b8cc600a9d761890d69bc541cdbc46f1075
jorge gomez <Jorge96js>
2023-02-27 21:02:57 -0300

    27 de febrero

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
-const {src, dest, watch} = require("gulp")
+const {src, dest, watch, parallel} = require("gulp")
 
-const sass = require("gulp-sass")(require("sass"))
+//CSS
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require ('gulp-plumber');
 
-const plumber = require ('gulp-plumber')
+//imagenes
+const cache = require('gulp-cache')
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(cb){
 
@@ -18,12 +24,50 @@ function css(cb){
     cb();
 };
 
-function dev(cb){
+function imagenes(cb){
+    
+    const opciones ={
+        optimizationLevel: 3
+    }
 
-    watch("src/scss/**/*.scss", css)
+    src('src/img/**/*')
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest('build/img'))
     cb()
 }
 
-exports.css = css
 
-exports.dev = dev
+function versionWebp(cb){
+
+    const options = {
+        quality: 50
+    };
+    src('src/img/**/*')
+    .pipe(webp(options))
+    .pipe(dest('build/img'))
+    
+    cb();
+}
+
+
+function javascritp(cb){
+    src('src/js/**/*.js')
+    .pipe(dest('build/js'))
+
+    cb()
+}
+
+function dev(cb){
+
+    watch("src/scss/**/*.scss", css)
+    watch("src/js/**/*.js", javascritp)
+    cb()
+}
+
+
+
+exports.css = css
+exports.js = javascritp
+exports.versionWebp = versionWebp
+exports.imagenes = imagenes
+exports.dev = parallel(imagenes, versionWebp, javascritp, dev)
