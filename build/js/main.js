d99f8b0bdc5ddef934dc130551d7fafed9102fc1
jorge gomez <Jorge96js>
2023-03-02 11:36:53 -0300

    agregando galeria

--- build/js/main.js
+++ build/js/main.js
@@ -9,5 +9,51 @@ function iniciarApp(){
 function crearGaleria(){
     const galeria = document.querySelector('.galeria-imagenes')
 
+    for(let i = 1; i<= 12; i++){
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+        <source srcset="build/img/thumb/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="Vocalista tocando la pala'nt">
+        `
+
+        imagen.onclick = function(){
+            mostrarImagen(i);
+        }
+        galeria.appendChild(imagen)
+        }
+}
+
+function mostrarImagen(id){
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+    <source srcset="build/img/grande/${id}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="Vocalista tocando la pala'nt">
+    `
+    //crea overlay
+    const overlay = document.createElement('div')
+    overlay.appendChild(imagen)
+    overlay.classList.add('overlay')
+    overlay.onclick = function(){
+        body.appendChild(overlay)
+        body.classList.remove('fijar-body')
+        overlay.remove();
+    }
     
+    //cerrar modal
+
+    const cerrarModal = document.createElement('p')
+    cerrarModal.textContent = "X"
+    cerrarModal.onclick = function(){
+        body.appendChild(overlay)
+        body.classList.remove('fijar-body')
+        overlay.remove();
+    }
+    cerrarModal.classList.add("btn-cerrar")
+
+    overlay.appendChild(cerrarModal)
+    
+    //añade al body
+    const body = document.querySelector('body')
+    body.appendChild(overlay)
+    body.classList.add('fijar-body')
 }
