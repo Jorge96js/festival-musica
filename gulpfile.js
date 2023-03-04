715cbe554e5ace24395c4dbe3ced7f19640f9b13
jorge gomez <Jorge96js>
2023-03-04 14:13:12 -0300

    proyecto finalizado

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,12 @@ const {src, dest, watch, parallel} = require("gulp")
 //CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require ('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+
+const terser = require('gulp-terser-js')
 
 //imagenes
 const cache = require('gulp-cache')
@@ -17,8 +23,11 @@ function css(cb){
     //almacenar en el disco
 
     src("src/scss/**/*.scss")
+    .pipe(sourcemaps.init())
     .pipe(plumber())
     .pipe(sass())
+    .pipe( postcss([ autoprefixer(), cssnano() ]) )
+    .pipe(sourcemaps.write('.'))
     .pipe(dest("build/css"))
 
     cb();
@@ -52,6 +61,9 @@ function versionWebp(cb){
 
 function javascritp(cb){
     src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
     .pipe(dest('build/js'))
 
     cb()
@@ -70,4 +82,4 @@ exports.css = css
 exports.js = javascritp
 exports.versionWebp = versionWebp
 exports.imagenes = imagenes
-exports.dev = parallel(imagenes, versionWebp, javascritp, dev)
+exports.dev = parallel(css,imagenes, versionWebp, javascritp, dev)
